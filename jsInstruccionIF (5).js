5b237afc664b901af398549de83f7f47569b7482
juanneves99 <juanneves99@hotmail.com>
2017-02-09 20:38:24 -0300

    Add files via upload

---
+++ jsInstruccionIF (5).js
@@ -0,0 +1,31 @@
+function Mostrar()
+{
+//tomo la edad  
+/*	var edad;
+	edad=document.getElementById('edad').value;
+	if(edad<18&&edad>12)
+	{
+		alert("Es adolescente");
+	}
+	else
+	{
+		alert("No es adolescente");
+	}
+*/
+	/*var edad;
+	edad=document.getElementById('edad').value;
+	if(!(edad<18&&edad>12))
+	{
+		alert("No es adolescente");
+	}*/
+
+	var edad;
+	edad=document.getElementById('edad').value;
+	if(edad>=18||edad<12)
+	{
+		alert("No es adolescente");
+	}
+	
+
+
+}//FIN DE LA FUNCIÓN
