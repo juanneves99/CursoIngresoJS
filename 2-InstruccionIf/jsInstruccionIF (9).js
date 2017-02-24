154c42d066e114ecc8a35c8431abc72cba7c7c2e
juanneves99 <juanneves99@hotmail.com>
2017-02-24 20:17:10 -0300

    Update jsInstruccionIF (9).js

--- 2-InstruccionIf/jsInstruccionIF (9).js
+++ 2-InstruccionIf/jsInstruccionIF (9).js
@@ -1,6 +1,7 @@
 function Mostrar()
 {
 	//Genero el número RANDOM entre 1 y 10 
-	
+	var minumeroRandom=Math.floor((Math.random() * 10) + 1);
+	minumeroRandom=document.getElementById(m)
 
 }//FIN DE LA FUNCIÓN
