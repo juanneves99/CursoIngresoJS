efe3d0a562e543ca81b17a0d73decd3706802f0a
juanneves99 <juanneves99@hotmail.com>
2017-02-24 20:29:01 -0300

    Update jsInstruccionSwitch(2).js

--- 3-InstruccionSwitch/jsInstruccionSwitch(2).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(2).js
@@ -2,8 +2,27 @@ function Mostrar()
 {
 //tomo la edad  
 var mesDelAño = document.getElementById('mes').value;
+switch(mesDelAño)
+{
+	case "Julio":
+	case "Agosto":
+	alert("Abrigate que hace frio !");
+	break;
+	case "Enero":
+	case "Febrero":
+	case "Marzo":
+	case "Abril":
+	case "Mayo":
+	case "Junio":
+	alert("Falta para el invierno !");
+	break;
+	case "Septiembre":
+	case "Octubre":
+	case "Noviembre":
+	case "Diciembre":
+	alert("Ya pasamos el frio, ahora calor !");
+}
 
-alert (mesDelAño);
 
 
 
