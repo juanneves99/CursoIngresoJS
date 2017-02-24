6fe3ee356fe9676edbec7624447cd45ffdd3ca4d
juanneves99 <juanneves99@hotmail.com>
2017-02-24 19:54:35 -0300

    Add files via upload

--- 6-InstruccionFor/jsIteracionesFor (2).js
+++ 6-InstruccionFor/jsIteracionesFor (2).js
@@ -1,5 +1,52 @@
 function Mostrar()
-{
+{	
+	var mascota;
+	var cont=0;
+	var respuesta="si";
+	var contMascotaSi=0;
+	var contMascotaNo=0;
+	var tipoDeMascota;
+	var edad;
+	var edadMayor;
 
+	for( ;cont<100 ; ) 
+	{
+		mascota=prompt("Tiene mascota ?"+cont);
+		while(respuesta!="si" && respuesta!="no")
+		{
+			mascota=prompt("Eso no viene al caso!!, ¿ tiene mascota ?");
+		}
+
+		if(respuesta="si")
+		{
+			contMascotaSi++;
+			tipoDeMascota=prompt("Que tipo de mascota es ?");
+
+			while(tipoDeMascota!="perro" && tipoDeMascota!="gato")
+			{
+				tipoDeMascota=prompt("Tipo de mascota incorrecto, que tipo de mascota es ?");
+
+			}
+
+		}
+		else
+		{
+						
+			if(respuesta="no")
+			{
+				contMascotaNo++;
+				edad=prompt("Que edad tiene ?");
+			}
+
+			
+
+			while(edadMayor)
+			{
+				edad=prompt("Usted es viejo/a");
+			}
+		}
+
+		cont++;
+	}
 
 }
