d74fb79339dc2603df659106c7c8614aedb769c9
juanneves99 <juanneves99@hotmail.com>
2017-02-24 20:18:33 -0300

    Delete jsInstruccionIF (9).js

--- jsInstruccionIF (9).js
+++
@@ -1,7 +0,0 @@
-function Mostrar()
-{
-	//Genero el número RANDOM entre 1 y 10 
-	var minumeroRandom=Math.floor((Math.random() * 10) + 1);
-	minumeroRandom=document.getElementById(m)
-
-}//FIN DE LA FUNCIÓN
