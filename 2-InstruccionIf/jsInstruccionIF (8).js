0217ebfb803b8b9f451b898e328e333f4c259d85
juanneves99 <juanneves99@hotmail.com>
2017-02-24 20:16:35 -0300

    Update jsInstruccionIF (8).js

--- 2-InstruccionIf/jsInstruccionIF (8).js
+++ 2-InstruccionIf/jsInstruccionIF (8).js
@@ -1,7 +1,14 @@
 function Mostrar()
 {
 //tomo la edad  
-
+	var edad;
+	var estadoCivil;
+	edad=document.getElementById('edad').value;
+	estadoCivil=document.getElementById('estadoCivil').value;
+	if(edad<18)
+	{
+		if(!(estadoCivil=="Soltero"))
+	}
 	
 
 
