42cee1e4e7d1bb0dc347799b8d33affa7ec6f7f6
juanneves99 <juanneves99@hotmail.com>
2017-02-24 20:15:51 -0300

    Update jsInstruccionIF (6).js

--- 2-InstruccionIf/jsInstruccionIF (6).js
+++ 2-InstruccionIf/jsInstruccionIF (6).js
@@ -1,8 +1,21 @@
+
 function Mostrar()
 {
 //tomo la edad  
 
-
-
+	var edad;
+	edad=document.getElementById('edad').value;
+	if(edad>17)
+	{
+		alert("Mayor de edad");
+	}
+	if(edad>12&&edad<18)
+	{
+		alert("Adolescente");
+	}
+	if(edad<12)
+	{
+		alert("Niño");
+	}
 
 }//FIN DE LA FUNCIÓN
