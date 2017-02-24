704003f98ba1f59348f1e710eeae6659608dcb82
juanneves99 <juanneves99@hotmail.com>
2017-02-24 20:29:48 -0300

    Update jsInstruccionSwitch(4).js

--- 3-InstruccionSwitch/jsInstruccionSwitch(4).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(4).js
@@ -1,8 +1,30 @@
 function Mostrar()
 {
 //tomo la edad  
-var mesDelAño = document.getElementById('mes').value;
+	var mesDelAño;
+	mesDelAño = document.getElementById('mes').value;
 
+	switch(mesDelAño)
+	{
+		case "Febrero":
+		alert("Si tiene 28 dias");
+		break;
+		case "Enero":
+		case "Marzo":
+		case "Mayo":
+		case "Julio":
+		case "Agosto":
+		case "Octubre":
+		case "Diciembre":
+		alert("Si tiene 31 dias");
+		break;
+		case "Abril":
+		case "Junio":
+		case "Septiembre":
+		case "Noviembre":
+		alert("Si tiene 30 dias");
+		break;
+	}
 //alert (mesDelAño);
 	
 	
