567d7b8de5d781d27b03ce2967e36bb7e813bdc2
juanneves99 <juanneves99@hotmail.com>
2017-02-24 19:46:43 -0300

    Add files via upload

--- 1-EntradaSalida/jsEntradaSalida-2.js
+++ 1-EntradaSalida/jsEntradaSalida-2.js
@@ -1,7 +1,13 @@
 /*Debemos lograr tomar un nombre con 'prompt' 
-y luego mostrarlo por 'alert' al presionar el botón  'MOSTRAR'*/
-function Mostar()
-{
-	
-}
-
+ y luego mostrarlo por 'alert' al presionar el botón  'MOSTRAR'*/
+ function Mostar()
+ {
+ 	var nombre
+     nombre = "Juan";
+     nombre = prompt ("Ingresa tu nombre");
+     alert (nombre);
+ 
+ 
+ 	var person = prompt("Ingresa tu nombre", "Natalia Natalia");
+ }
+ 
