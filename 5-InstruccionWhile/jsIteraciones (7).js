cfad15c363c818af326fffd5bddca20429c808db
juanneves99 <juanneves99@hotmail.com>
2017-02-24 21:45:09 -0300

    Update jsIteraciones (7).js

--- 5-InstruccionWhile/jsIteraciones (7).js
+++ 5-InstruccionWhile/jsIteraciones (7).js
@@ -4,6 +4,17 @@ function Mostrar()
 	var contador=0;
 	var acumulador=0;
 	var respuesta='si';
+	var numero;
+
+	while(respuesta=="si")
+	{
+		
+		numero=prompt("Ingrese su numero");
+		numero=parseInt(numero);
+		contador=contador+1;
+		acumulador=acumulador+numero;
+		respuesta=prompt("Ingrese si para continuar");
+	}
 
 
 document.getElementById('suma').value=acumulador;
