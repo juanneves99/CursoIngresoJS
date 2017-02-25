417ff32c69b330d3ab149a7485659af4faf31b80
juanneves99 <juanneves99@hotmail.com>
2017-02-24 21:44:40 -0300

    Update jsIteraciones (6).js

--- 5-InstruccionWhile/jsIteraciones (6).js
+++ 5-InstruccionWhile/jsIteraciones (6).js
@@ -3,6 +3,17 @@ function Mostrar()
 
 	var contador=0;
 	var acumulador=0;
+	var numero;
+	
+	while(contador<5)
+	{
+		
+		numero=prompt("Ingrese su numero");
+		numero=parseInt(numero);
+		contador=contador+1;
+		acumulador=acumulador+numero;
+		
+	}
 
 
 
