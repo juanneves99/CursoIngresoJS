af050bc695a09d460cf77542533efa02bcb901dc
juanneves99 <juanneves99@hotmail.com>
2017-02-24 21:43:23 -0300

    Update jsIteraciones (4).js

--- 5-InstruccionWhile/jsIteraciones (4).js
+++ 5-InstruccionWhile/jsIteraciones (4).js
@@ -1,7 +1,14 @@
+
 function Mostrar()
 {
 
 	var numero = prompt("ingrese un número entre 0 y 10.");
+
+	while(numero<0 || >10)
+	{
+		numero = prompt("ingrese un número entre 0 y 10.");
+	}
+	numero = prompt("Reingrese el número");
 
 
 }//FIN DE LA FUNCIÓN
