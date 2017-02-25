0e0067c26cb5c58bbf607bb191993e7c54716ed0
juanneves99 <juanneves99@hotmail.com>
2017-02-24 21:43:54 -0300

    Update jsIteraciones (5).js

--- 5-InstruccionWhile/jsIteraciones (5).js
+++ 5-InstruccionWhile/jsIteraciones (5).js
@@ -1,10 +1,16 @@
 function Mostrar()
+
+function Mostrar()
 {
 
-var sexo = prompt("ingrese f ó m .");
+	var sexo = prompt("ingrese f ó m .");
 
+	while(sexo!= "f" && "m")
+	{
+		sexo = prompt("reingrese f ó m .");
+	}
 
-
-document.getElementById('Sexo').value=sexo;
+	document.getElementById('Sexo').value=sexo;
+	alert("El sexo es "+sexo);
 
 }//FIN DE LA FUNCIÓN
